f24790d2ae2c7bb5667ff7e0e010839974f51066
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2023-12-09 17:15:31 -0500

    Completed code Along

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+// routes.js
+import App from "./App";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -6,25 +8,78 @@ import ErrorPage from "./pages/ErrorPage";
 
 const routes = [
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+           {
+              path: "/",
+              element: <Home />,
+              children: [
+                  {
+                      path: "/profile/:id",
+                      element: <UserProfile />
+                  }
+              ]
+          }, 
+          {
+              path: "/about",
+              element: <About />
+          },
+          {
+              path: "/login",
+              element: <Login />
+          }
+      ]
   }
 ];
 
 export default routes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Login from "./pages/Login";
+// import UserProfile from "./pages/UserProfile";
+// import ErrorPage from "./pages/ErrorPage";
+
+// const routes = [
+//   {
+//     path: "/",
+//     element: <Home />,
+//     errorElement: <ErrorPage />
+//   }, 
+//   {
+//     path: "/about",
+//     element: <About />,
+//     errorElement: <ErrorPage />
+//   },
+//   {
+//     path: "/login",
+//     element: <Login />,
+//     errorElement: <ErrorPage />
+//   },
+//   {
+//     path: "/profile/:id",
+//     element: <UserProfile />,
+//     errorElement: <ErrorPage />
+//   }
+// ];
+
+// export default routes;
